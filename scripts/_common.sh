a2ceb3fe0ab9612af73bf1e5c30300975a561844
Sebastian Gumprich <github@gumpri.ch>
2020-12-21 21:28:09 +0100

    add more dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,11 @@
 # COMMON VARIABLES
 #=================================================
 
+YNH_PHP_VERSION="7.4"
+nodejs_version=12
+
 # dependencies used by the app
-pkg_dependencies="phantomjs"
+pkg_dependencies="phantomjs php7.4-zip php7.4-gmp php7.4-gd php7.4-bcmath php7.4-fileinfo php7.4-json php7.4-mbstring php7.4-pdo php7.4-tokenizer php7.4-xml php7.4-curl php7.4-mysql gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget"
 
 #=================================================
 # PERSONAL HELPERS
